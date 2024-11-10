eac0256d14a3223e1dba48960544a701818f1944
Alexander Nadvotsky <nadvotsky@tuta.com>
2024-11-10 12:10:44 +0000

    feat(filters): add relative_indent filter
    This filter plugin is intended to solve an indentation problem for
    jinja templates by adding the required number of tabs or spaces to all
    lines except the first one.

---
+++ filter_plugins/extras.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import inspect
+
+
+class FilterModule:
+    def filters(self):
+        def method_filter(m):
+            return (
+                (inspect.isfunction(m) or inspect.ismethod(m))
+                and not m.__name__.startswith("_")
+                and not m.__name__ == self.filters.__name__
+            )
+
+        return dict(inspect.getmembers(self, method_filter))
+
+    #
+    # `relative_indent` is a lightweight filter plugin that splits the specified string by lines, filters them, and adds
+    #  the required intendation.
+    #
+    @staticmethod
+    def relative_indent(string, tab=False, space=0, times=0):
+        lines = string.strip().splitlines()
+        prepend = ("\t" if tab else (" " * space)) * times
+
+        mask = [False if i == 0 else bool(line.strip()) for i, line in enumerate(lines)]
+        if not any(mask):
+            return string.strip()
+
+        block_base = min([
+            len(lines[i]) - len(lines[i].lstrip()) for i, m in enumerate(mask) if m is True
+        ])
+
+        return "\n".join([
+            (prepend + lines[i][block_base:]) if m else lines[i]
+            for i, m in enumerate(mask)
+        ])
+
