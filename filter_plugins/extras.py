8f27d52baef48f310cc5cb69d457002ecb56c207
Alexander Nadvotsky <nadvotsky@tuta.com>
2024-11-10 21:53:05 +0000

    feat(filters): add parse_ansi_preset filter
    This filter add a full-featured ANSI color parser that can be used
    inside templates. This unlocks theming support for terminal-based
    applications directly from inventory, following main theme declaration.
    
    Since different application may call ANSI colors differently, filter
    supports custom modifier, separator, and replacements overlay. It also
    includes full color descriptor in order to make it can be used in
    various templates.

--- filter_plugins/extras.py
+++ filter_plugins/extras.py
@@ -37,6 +37,48 @@ class FilterModule:
         ])
 
 
+    DEFAULT_REPLACEMENTS = dict(enumerate(["black", "red", "green", "yellow", "blue", "purple", "cyan", "white"]))
+    ANSI_TABLE_SIZE = 8
+
+    @classmethod
+    def _parse_ansi_preset(cls, item_group, item, lookup, code_to_name, mod, sep):
+        try:
+            mode_alt = False
+            position = lookup.index(item)
+            ansi = (30 if position < cls.ANSI_TABLE_SIZE else 90) + position % cls.ANSI_TABLE_SIZE
+            name = code_to_name[position % cls.ANSI_TABLE_SIZE]
+            modifier = mod if position >= cls.ANSI_TABLE_SIZE else None
+        except ValueError:
+            mode_alt = True
+            position = None
+            ansi = None
+            name = item
+            modifier = None
+
+        return {
+            "mode_alt": mode_alt,
+            "modifiers": modifier,
+            "raw_key": position,
+            "ansi": ansi,
+            "key": name,
+            "line": sep.join(filter(None, [modifier, name])),
+        }
+
+    @classmethod
+    def parse_ansi_presets(cls, preset, ansi, replacements={}, mod="bright", sep="-"):
+        assert all(
+            (len(item) == cls.ANSI_TABLE_SIZE for item in ansi.values())
+        ), "Size of the ansi table must be exactly 8 elements"
+
+        code_to_name = {**cls.DEFAULT_REPLACEMENTS, **replacements}
+        lookup = ansi["normal"] + ansi["bright"]
+
+        return {
+            name: parsed
+            for name, color in preset.items()
+            if (parsed := cls._parse_ansi_preset(name, color, lookup, code_to_name, mod, sep)) is not None
+        }
+
     @staticmethod
     def is_tagged(tags, ansible_run_tags, ansible_skip_tags):
         tags, ansible_run_tags, ansible_skip_tags = map(frozenset, (tags, ansible_run_tags, ansible_skip_tags))
