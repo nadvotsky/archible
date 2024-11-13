f70c0f1e0434aa18f701f4f6885e439351a67a9f
Alexander Nadvotsky <nadvotsky@tuta.com>
2024-11-13 02:12:34 +0000

    feat(filters): add keyboard bindings parser filter
    parse_binds is a filter plugin that allows to unify keyboard bindings
    across different programs via templates.
    
    Firstly, it requires defining required modifiers, i.e. supported
    keyboard contexts such as CTRL, SUPER, or emacs. Secondly, a mapping
    of possible binds denotes the bindings itself that uses previously
    defined modifiers.
    
    The filter interface operates on bare dictionary types and allows to
    overlay inventory setups for application-specific quirks. It also
    features resulting bindings customization, including custom separators
    and shift merging. Templates are capable of accessing detailed metadata
    of each processed key binding.

--- filter_plugins/extras.py
+++ filter_plugins/extras.py
@@ -36,6 +36,128 @@ class FilterModule:
             for i, m in enumerate(mask)
         ])
 
+    @staticmethod
+    def _parse_bind_mode(bind):
+        mode, alt, key = None, False, []
+
+        for expr in bind.split(" "):
+            if expr == ":alt":
+                alt = True
+            elif expr.startswith(":"):
+                mode = expr.lstrip(":")
+            else:
+                key.append(expr)
+
+        assert len(key) == 1, f"bind expression must contain exactly one key ({bind})"
+        assert bool(mode) is True, "bind expression must contain the name of the mode"
+
+        return mode, alt, key[0]
+
+    @staticmethod
+    def _parse_bind_modifiers(mode, alt, modifiers, action):
+        bind_modifiers = {
+            "super": False,
+            "alt": False,
+            "ctrl": False,
+            "shift": False,
+        }
+
+        exprs = [
+            modifiers[mode]["alt" if alt else "main"],
+            *[
+                modifiers["extra"][k]
+                for k in ["word", "many", "select"]
+                if action[k] is True
+            ]
+        ]
+
+        for expr in filter(None, exprs):
+            for modifier in expr.split("+"):
+                bind_modifiers[modifier.lower()] = True
+
+        return bind_modifiers
+
+    @staticmethod
+    def _parse_bind_key(key, keys_map, shift, merge_shift):
+        if not (shift and merge_shift):
+            return False, keys_map.get(key, key)
+
+        def shift_pipe(k):
+            if k is None:
+                return None
+            elif len(k) == 1 and k.islower():
+                return chr(ord(key) - 32)
+
+            return {
+                "1": "!", "2": "@", "3": "#", "4": "$", "5": "%",
+                "6": "^", "7": "&", "8": "*", "9": "(", "0": ")",
+                "-": "_", "=": "+", ";": ":", "'": "\"", "[": "{",
+                "]": "}", "\\": "|", ",": "<", ".": ">", "/": "?",
+                "Tilde": "~",
+            }.get(key)
+
+        replacement = shift_pipe(key) or shift_pipe(keys_map.get(key))
+        new_key = replacement or key
+        return replacement is not None, keys_map.get(new_key, new_key)
+
+    @classmethod
+    def parse_bind(
+        cls,
+        bind,
+        user_action,
+        keys,
+        modifiers_map={},
+        keys_map={},
+        sep=("+", " "),
+        merge_shift=False
+    ):
+        action = {"name": None, "subs": None, "word": False, "many": False, "select": False}
+        if isinstance(user_action, dict):
+            action.update(user_action)
+        else:
+            action["subs"] = user_action
+
+        mode, alt, raw_key = cls._parse_bind_mode(bind)
+        modifiers = cls._parse_bind_modifiers(mode, alt, keys["modifiers"], action)
+        is_merged, key = cls._parse_bind_key(raw_key, keys_map, modifiers["shift"], merge_shift)
+
+        modifiers_line = sep[0].join([
+            modifiers_map.get(modifier, modifier)
+            for modifier, status in modifiers.items()
+            if status is True and not (is_merged is True and modifier == "shift")
+        ])
+
+        return {
+            "action": action,
+            "mode_name": mode,
+            "mode_alt": alt,
+            "raw_key": raw_key,
+            "raw_modifiers": modifiers,
+            "key": key,
+            "modifiers": modifiers_line,
+            "merge_shift": merge_shift,
+            "line": sep[1].join(filter(None, (modifiers_line, key))),
+        }
+
+    @classmethod
+    def parse_binds(
+        cls,
+        keys,
+        actions_map,
+        modifiers_map={},
+        keys_map={},
+        sep=("+", " "),
+        merge_shift=False,
+    ):
+        for action_name, user_action in actions_map:
+            action = {"name": action_name}
+            if isinstance(user_action, dict):
+                action.update(user_action)
+            else:
+                action["subs"] = user_action
+
+            for bind in keys["binds"][action_name]:
+                yield cls.parse_bind(bind, action, keys, modifiers_map, keys_map, sep, merge_shift)
 
     DEFAULT_REPLACEMENTS = dict(enumerate(["black", "red", "green", "yellow", "blue", "purple", "cyan", "white"]))
     ANSI_TABLE_SIZE = 8
