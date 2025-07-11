dc01e5c76a0d122eafae7887337f59290d099943
Alexander Nadvotsky <nadvotsky@tuta.com>
2025-07-11 18:32:12 +0000

    docs(global): rephrase inventory and document filters

--- roles/aa-global/aa-shared/filter_plugins/aa_newline.py
+++ roles/aa-global/aa-shared/filter_plugins/aa_newline.py
@@ -1,3 +1,9 @@
+#
+# `aa_newline` represents a jinja filter that appends a newline to a source string.
+#
+# The rationale behind is that sometimes YAML-escaping gets a little tricky to maintain, and requires double-escaping
+#  \\\\n. For all other cases, it adheres to for more concise syntax instead of `(str + "\n")` or `("{}\n".format(str))`.
+#
 class FilterModule:
     def filters(self):
         return dict(aa_newline=self.newline)
--- roles/aa-global/aa-shared/filter_plugins/aa_relindent.py
+++ roles/aa-global/aa-shared/filter_plugins/aa_relindent.py
@@ -1,3 +1,29 @@
+#
+# `aa_relindent` is a jinja filter that can be used in complicated configuration rendering scenarios, where underlying
+#  block needs to be properly indented.
+#
+# Due to the jinja being mainly focused on non-permissive markup languages such as HTML, no additional indentaion
+#  control features are available. In case of strict spacing demands such as configuration scripts, this may bring some
+#  complications.
+#
+# `aa_relindent` seeks the most lightweight, in-place solution and does not solve the problem entirely. It allows to use
+#  the existing codebase without major modifications. For more radical prototype, take a look at the following:
+#   - foundation.legacy.template_plus
+#   - https://github.com/stereobutter/jinja2_workarounds
+#   - https://github.com/dldevinc/jinja2-indent
+#
+# This filter basically splits the specified string by lines, filters them, and adds the required intendation.
+# For example:
+#
+# <space>config_entry = true
+# <space>{{ "one = true\ntwo = false" | aa_relindent(space=4, times=1) }}
+#
+# Is going to be rendered as:
+#
+# <space>config_entry = true
+# <space>one = true
+# <space>two = true
+#
 class FilterModule:
     def filters(self):
         return dict(aa_relindent=self.relative_indent)
