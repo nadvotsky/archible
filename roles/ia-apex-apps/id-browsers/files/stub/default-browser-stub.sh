f51d8043b6ea992088d2fe5fa879b075a613e399
Alexander Nadvotsky <nadvotsky@tuta.com>
2025-07-29 19:40:14 +0000

    fix: WAYLAND_SOCKET may not be defined in Wayland sessions

--- roles/ia-apex-apps/id-browsers/files/stub/default-browser-stub.sh
+++ roles/ia-apex-apps/id-browsers/files/stub/default-browser-stub.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ -n "$WAYLAND_SOCKET" ]] && [[ -n "$WAYLAND_DISPLAY" ]]; then
+if [[ -n "$WAYLAND_DISPLAY" ]]; then
 	wl-copy "$1"
 else
 	echo -n "$1" | xsel -b
 fi
 
 notify-send \
-	--appname="Default Application Stub" \
+	--app-name="Default Application Stub" \
 	--icon=applications-internet \
 	--urgency=low \
 	"Clicked link has been copied to the clipboard" \
